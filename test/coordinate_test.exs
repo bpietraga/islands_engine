e5fc5a53baeecc96fa87d62b181eb06d7c579e1f
Bernard Pietraga <bernard.pietraga@gmail.com>
2017-07-20 21:16:50 +0200

    Add coordinate struct

---
+++ test/coordinate_test.exs
@@ -0,0 +1,19 @@
+defmodule IslandsEngine.CoordinateTest do
+  use ExUnit.Case
+  doctest IslandsEngine
+
+  test "valid coordinate" do
+    expected = IslandsEngine.Coordinate.new(1, 1)
+    assert expected  == {:ok, %IslandsEngine.Coordinate{col: 1, row: 1}}
+  end
+
+  test "invalid row with minus position" do
+    expected = IslandsEngine.Coordinate.new(-1, 1)
+    assert expected  == {:error, :invalid_coordinate}
+  end
+
+  test "invalid row with to high position" do
+    expected = IslandsEngine.Coordinate.new(11, 1)
+    assert expected  == {:error, :invalid_coordinate}
+  end
+end
